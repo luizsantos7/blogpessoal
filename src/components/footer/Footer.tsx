e2849aca9e552555e6054e958dcd76b0550badd7
luizsantos7 <lhsantos098@gmail.com>
2025-06-23 15:09:58 -0300

    incrementação do axios e dos route

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,35 @@
+import { FacebookLogo } from "@phosphor-icons/react/dist/ssr/FacebookLogo"
+import { InstagramLogo } from "@phosphor-icons/react/dist/ssr/InstagramLogo"
+import { LinkedinLogo } from "@phosphor-icons/react/dist/ssr/LinkedinLogo"
+
+function Footer() {
+
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Generation | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/in/luizhenrique-dev/" target="_blank">
+                        <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/luizsantos7_/" target="_blank">
+                        <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/luishenrique.santos.735507/?locale=pt_BR" target="_blank">
+                        <FacebookLogo size={48} weight='bold' />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Footer
